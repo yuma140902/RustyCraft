886d1b6dd0af45d1a45d8d26e3ef80a9d742e517
yuma140902 <yuma140902@gmail.com>
2021-08-25 08:21:25 +0900

    merge

---
+++ lib/gl/target/debug/build/log-a8d38d431da9e5e4/build_script_build-a8d38d431da9e5e4.d
@@ -0,0 +1,5 @@
+C:\Users\yuma1\pj\rust\glsandbox\lib\gl\target\debug\build\log-a8d38d431da9e5e4\build_script_build-a8d38d431da9e5e4.exe: C:\Users\yuma1\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\log-0.4.14\build.rs
+
+C:\Users\yuma1\pj\rust\glsandbox\lib\gl\target\debug\build\log-a8d38d431da9e5e4\build_script_build-a8d38d431da9e5e4.d: C:\Users\yuma1\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\log-0.4.14\build.rs
+
+C:\Users\yuma1\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\log-0.4.14\build.rs:
---
+++ lib/gl/target/debug/deps/cfg_if-1adc94b865e20dc3.d
@@ -0,0 +1,7 @@
+C:\Users\yuma1\pj\rust\glsandbox\lib\gl\target\debug\deps\cfg_if-1adc94b865e20dc3.rmeta: C:\Users\yuma1\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\yuma1\pj\rust\glsandbox\lib\gl\target\debug\deps\libcfg_if-1adc94b865e20dc3.rlib: C:\Users\yuma1\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\yuma1\pj\rust\glsandbox\lib\gl\target\debug\deps\cfg_if-1adc94b865e20dc3.d: C:\Users\yuma1\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs
+
+C:\Users\yuma1\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\cfg-if-1.0.0\src\lib.rs:
