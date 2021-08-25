0e1165eb3c183aeb005f75e8c0281a64bdab5e47
yuma140902 <yuma140902@gmail.com>
2021-08-25 15:25:41 +0900

    remove target/

--- lib/gl/target/debug/build/khronos_api-9294e9f6f4585adb/build_script_build-9294e9f6f4585adb.d
+++
@@ -1,5 +0,0 @@
-C:\Users\yuma1\pj\rust\glsandbox\lib\gl\target\debug\build\khronos_api-9294e9f6f4585adb\build_script_build-9294e9f6f4585adb.exe: C:\Users\yuma1\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\khronos_api-3.1.0\build.rs
-
-C:\Users\yuma1\pj\rust\glsandbox\lib\gl\target\debug\build\khronos_api-9294e9f6f4585adb\build_script_build-9294e9f6f4585adb.d: C:\Users\yuma1\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\khronos_api-3.1.0\build.rs
-
-C:\Users\yuma1\scoop\persist\rustup-msvc\.cargo\registry\src\github.com-1ecc6299db9ec823\khronos_api-3.1.0\build.rs:
