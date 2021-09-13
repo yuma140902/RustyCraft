d0bd6b9dd7249322ce2e30e2c8563fdbaa9dc027
yuma140902 <yuma140902@gmail.com>
2021-09-13 13:28:23 +0900

    目の高さを変更

--- src/camera_computer.rs
+++ src/camera_computer.rs
@@ -9,7 +9,13 @@ impl CameraComputer {
         CameraComputer {}
     }
 
+    const PLAYER_EYE_DIFF: nalgebra::Vector3<f32> = nalgebra::Vector3::new(0.0, 0.3, 0.0);
+
     pub fn compute_view_matrix(&self, angle: &Angle2, pos: &Position) -> Matrix4<f32> {
-        Matrix4::<f32>::look_at_rh(&pos.0, &(pos.0 + angle.front()), &angle.up())
+        Matrix4::<f32>::look_at_rh(
+            &(pos.0 + CameraComputer::PLAYER_EYE_DIFF),
+            &(pos.0 + angle.front()),
+            &angle.up(),
+        )
     }
 }
