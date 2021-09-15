c4204d53b5d207c0e1bad148bc5ddc47fd996488
yuma140902 <yuma140902@gmail.com>
2021-09-15 13:18:18 +0900

    重力加速度を定数化

--- src/components/acceleration.rs
+++ src/components/acceleration.rs
@@ -1,12 +1,14 @@
 use nalgebra::Vector3;
 use specs::{Component, VecStorage};
 
+use crate::game_config;
+
 #[derive(Component, Debug)]
 #[storage(VecStorage)]
 pub struct Acceleration(pub Vector3<f32>);
 
 impl Acceleration {
     pub fn gravity() -> Acceleration {
-        Acceleration(Vector3::new(0.0, -0.0001, 0.0))
+        Acceleration(Vector3::new(0.0, -game_config::GRAVITY, 0.0))
     }
 }
--- src/game_config.rs
+++ src/game_config.rs
@@ -1,2 +1,3 @@
 pub const ROTATE_SPEED: f32 = 0.015f32;
 pub const MOVE_SPEED: f32 = 0.002f32;
+pub const GRAVITY: f32 = 0.00001;
