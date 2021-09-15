997f79761c64f5f3fc66d88d601bedd64fc29bed
yuma140902 <yuma140902@gmail.com>
2021-09-15 13:29:39 +0900

    force is deprecated

--- src/components/force.rs
+++ src/components/force.rs
@@ -5,6 +5,7 @@ use crate::ecs_resources::DeltaTick;
 
 #[derive(Component, Debug, Default)]
 #[storage(VecStorage)]
+#[deprecated]
 pub struct Force {
     pub vec: Vector3<f32>,
     pub ticks: DeltaTick,
